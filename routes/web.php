edf3423f23620c63da7311d19078126a03079450
ameRese <yarukix@gmail.com>
2024-08-06 21:51:55 +0900

    feat: 投稿データ保存画面追加

--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,15 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('post/create', [PostController::class, 'create']);
+
+Route::post('post', [PostController::class, 'store'])->name('post.store');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
