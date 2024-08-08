9b42d74da3314f3ca9ace201d4b305365dc5feb6
ameRese <yarukix@gmail.com>
2024-08-08 22:52:19 +0900

    feat: リソースコントローラー追加

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,21 +2,33 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 use App\Models\Post;
+use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
-    public function index() {
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
         $posts = Post::all();
         return view('post.index', compact('posts'));
     }
-    
-    public function create() {
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
         return view('post.create');
     }
 
-    public function store(Request $request) {
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
         $validated = $request->validate([
             'title' => 'required|max:20',
             'body' => 'required|max:400',
@@ -25,18 +37,30 @@ class PostController extends Controller
         $post = Post::create($validated);
 
         $request->session()->flash('message', '保存しました');
-        return back();
+        return redirect()->route('post.index');
     }
 
-    public function show(Post $post) {
+    /**
+     * Display the specified resource.
+     */
+    public function show(Post $post)
+    {
         return view('post.show', compact('post'));
     }
 
-    public function edit(Post $post) {
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Post $post)
+    {
         return view('post.edit', compact('post'));
     }
 
-    public function update(Request $request, Post $post) {
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Post $post)
+    {
         $validated = $request->validate([
             'title' => 'required|max:20',
             'body' => 'required|max:400',
@@ -47,10 +71,14 @@ class PostController extends Controller
         $post->update($validated);
 
         $request->session()->flash('message', '更新しました');
-        return back();
+        return redirect()->route('post.show', compact('post'));
     }
 
-    public function destroy(Request $request, Post $post) {
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Request $request, Post $post)
+    {
         $post->delete();
         $request->session()->flash('message', '削除しました');
         return redirect()->route('post.index');
--- routes/web.php
+++ routes/web.php
@@ -4,26 +4,15 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
 
+Route::resource('post', PostController::class);
+
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('post/create', [PostController::class, 'create']);
-
-Route::post('post', [PostController::class, 'store'])->name('post.store');
-
-Route::get('post', [PostController::class, 'index'])->name('post.index');
-
-Route::get('post/show/{post}', [PostController::class, 'show'])->name('post.show');
-
-Route::get('post/{post}/edit', [PostController::class, 'edit'])->name('post.edit');
-Route::patch('post/{post}', [PostController::class, 'update'])->name('post.update');
-
-Route::delete('post/{post}', [PostController::class, 'destroy'])->name('post.destroy');
-
 Route::get('/dashboard', function () {
     return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+})->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
@@ -32,11 +21,3 @@ Route::middleware('auth')->group(function () {
 });
 
 require __DIR__.'/auth.php';
-
-// Language Switcher Route 言語切替用ルートだよ
-Route::get('language/{locale}', function ($locale) {
-    app()->setLocale($locale);
-    session()->put('locale', $locale);
-
-    return redirect()->back();
-});
